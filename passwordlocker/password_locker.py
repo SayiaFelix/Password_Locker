105525cb5dac4692520d2abdaf0f2d0aa5002596
SayiaFelix <sayia.lucas@student.moringaschool.com>
2022-04-23 10:24:34 +0300

    Testing for the save user

--- passwordlocker/password_locker.py
+++ passwordlocker/password_locker.py
@@ -1,5 +1,5 @@
 from requests import delete
-
+import pyperclip                #Importing pyperclip
 
 class User:
     '''
@@ -29,16 +29,15 @@ class User:
         '''
         User.user_list.remove(self)
 
-
     @classmethod
     def find_by_username(cls,username):
-        '''
-        Method that takes in a username and returns a user input that matches that username.
-        '''
-        for user in cls.user_list:
-            if user.username == username:
-                return user
-
+            '''
+            Method that takes in a username and returns a user input that matches that username.
+            '''
+            for user in cls.user_list:
+                if user.username == username:
+                    return user
+   
     
     @classmethod
     def display_user(cls):
@@ -46,3 +45,40 @@ class User:
         method that returns the user imput list
         '''
         return cls.user_list
+    @classmethod
+    def copy_password(cls,password):
+        password_found = Credential.find_by_username(password)
+        pyperclip.copy(password_found.password)
+
+#End of class User
+
+#Start of class Credential
+
+class Credential:
+    '''
+    Class that generate copy user credentials.
+    '''    
+    user_list=[]                             #Empty user list to store/save our username and password
+    
+    def __init__(self, username, password):
+
+        '''
+        Function for intantiating the user input for copying
+        '''
+        self.username= username
+        self.password= password
+
+
+    @classmethod
+    def find_by_username(cls,username):
+            '''
+            Method that takes in a username and returns a user input that matches that username.
+            '''
+            for user in cls.user_list:
+                if user.username == username:
+                    return user
+
+    @classmethod
+    def copy_password(cls,password):
+        password_found = Credential.find_by_username(password)
+        pyperclip.copy(password_found.password)
--- passwordlocker/password_locker_test.py
+++ passwordlocker/password_locker_test.py
@@ -1,5 +1,9 @@
 import unittest                             # Importing the unittest module
+import pyperclip                            # Importing the pyperclip module
 from password_locker import User            # Importing the User class from passwordlocker file
+from password_locker import Credential      # Importing the User class from passwordlocker file
+
+
 
 class TestUser(unittest.TestCase):
     '''
@@ -44,7 +48,6 @@ class TestUser(unittest.TestCase):
             self.new_user.save_user()
             test_user = User("Rensia","23455")           # new contact
             test_user.save_user()
-
             self.new_user.delete_user()                   # Deleting a user object
             self.assertEqual(len(User.user_list),1)
 
@@ -68,6 +71,17 @@ class TestUser(unittest.TestCase):
         self.assertEqual(User.display_user(),User.user_list)
 
     
+    def test_copy_password(self):
+        '''
+        Test to confirm that we are copying the password from the user imput
+        '''
+        self.new_user.save_user()
+        User.copy_password("23455")
+        self.assertEqual(self.new_user.password,pyperclip.paste())
+
+
+
+
 
 
 
