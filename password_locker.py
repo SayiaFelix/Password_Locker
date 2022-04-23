fc8a080b1fb74506dcb69e680aba968690bcf389
SayiaFelix <sayia.lucas@student.moringaschool.com>
2022-04-23 09:25:28 +0300

    Testing for the save user

--- password_locker.py
+++ password_locker.py
@@ -5,7 +5,18 @@ class User:
     user_list=[] #Empty user list to store/save our username and password
     
     def __init__(self, username, password):
+        
+        '''
+        Function for intantiating the user input
+        '''
+         
         self.username=username
         self.password= password
+
+    def save_user(self):     
+        '''
+        Function for saving username and password
+        '''
+        User.user_list.append(self)
 
 
--- password_locker_test.py
+++ password_locker_test.py
@@ -6,20 +6,19 @@ class TestUser(unittest.TestCase):
     Test class that defines test cases for the User class behaviours.
     '''
 
-    def setUp(self):
-          '''
-           Seting up method to run before test cases.
-          '''
+    def setUp(self):                      # Seting up method to run before test cases.
+        
+          self.new_user = User("Sir Felix","12345678",)  # create User instances
 
-          self.new_user = User("Sir Felix","12345678",) 
-
-          '''
-           create User instances
-          '''
-
-    def test_init(self): #Testing if the object has been initialized properply.
+      
+    def test_init(self):                                      #Testing if the object has been initialized properply.
         self.assertEqual(self.new_user.username,"Sir Felix")  
         self.assertEqual(self.new_user.password,"12345678")  
+    
+    def test_save_user(self):                      #To check if the user detatils has been saved correctly
+        self.new_user.save_user()              #For saving the user input
+        self.assertEqual(len(User.user_list),1)
+
 
 if __name__=="__main__":
     unittest.main()
